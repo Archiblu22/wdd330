eb3c898340997fe4d10b86b5a6461e7d5eac68bd
archiblue22 <paguirre@byui.edu>
2021-09-22 15:22:44 -0600

    commit

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
-const links = [
-  {
-    label: "Week1 notes",
-    url: "week1/index.html"
-  }
-]
+const linkList = ["/week01/index.html", "/week02/index.html"];
+const titleList = ["Week01", "Week02"];
+
+let weekLinks = document.querySelector('#weekLinks');
+
+linkList.forEach((x, i) => {
+  weekLinks.insertAdjacentHTML("beforeend", `<li><a href="${x}"><div>${titleList[i]}</div></a></li>`)
+})
